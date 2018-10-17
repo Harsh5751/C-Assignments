eba7ad68b2f0155aa9c8e24d9934a8d70d5a2a35
Harsh5751 <42149411+Harsh5751@users.noreply.github.com>
2018-10-16 23:05:51 -0400

    removing

--- Assignment2/Problem2/gettime.c
+++
@@ -1,9 +0,0 @@
-#include <sys/resource.h>
-#include <unistd.h>
-double gettime() {
-	struct rusage usage;
-	getrusage(RUSAGE_SELF, &usage);
-	struct timeval time;
-	time = usage.ru_utime;
-	return time.tv_sec + time.tv_usec/1e6;
-}
